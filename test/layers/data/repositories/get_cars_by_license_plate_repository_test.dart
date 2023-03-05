722acc72fc65f03bcf2e3f63f56ebe4b44d30791
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2023-03-05 19:55:25 -0300

    do test failed

---
+++ test/layers/data/repositories/get_cars_by_license_plate_repository_test.dart
@@ -0,0 +1,25 @@
+import 'package:clean_architecture_principles/layers/data/dto/car_dto.dart';
+import 'package:clean_architecture_principles/layers/domain/entities/car_entity.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+abstract class GetCarByLicensePlateRepository {
+  CarEntity call(String licensePlate);
+}
+
+class GetCarsByLicensePlateRepositoryImp
+    extends GetCarByLicensePlateRepository {
+  @override
+  CarEntity call(String licensePlate) {
+    if (licensePlate.contains("ABC123")) {
+      return CarDTO(plate: "ABC123", doors: 3, value: 5, color: "red");
+    }
+    return CarDTO(plate: "qwe123", color: '', doors: 3, value: 2);
+  }
+}
+
+void main() {
+  GetCarByLicensePlateRepository? repository;
+  test("Must return a entity car instance", () {
+    var result = repository!("ABC123");
+  });
+}
