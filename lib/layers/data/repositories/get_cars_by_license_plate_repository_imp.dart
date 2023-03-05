224a1d281e6d70f3a39eaaa6a85a9e62cfbba072
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2023-03-05 20:18:41 -0300

    Update get_cars_by_license_plate_repository_imp.dart

--- lib/layers/data/repositories/get_cars_by_license_plate_repository_imp.dart
+++ lib/layers/data/repositories/get_cars_by_license_plate_repository_imp.dart
@@ -1,12 +1,15 @@
 import 'package:clean_architecture_principles/layers/domain/entities/car_entity.dart';
 
 import '../../domain/repositories/get_cars_by_license_plate_repository.dart';
+import '../dto/car_dto.dart';
 
 class GetCarsByLicensePlateRepositoryImp
     extends GetCarByLicensePlateRepository {
   @override
   CarEntity call(String licensePlate) {
-    // TODO: implement call
-    throw UnimplementedError();
+    if (licensePlate.contains("ABC123")) {
+      return CarDTO(plate: "ABC123", doors: 3, value: 5, color: "red");
+    }
+    return CarDTO(plate: "qwe123", color: '', doors: 3, value: 2);
   }
 }
