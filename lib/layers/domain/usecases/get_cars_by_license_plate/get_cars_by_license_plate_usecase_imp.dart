7f5c231aac6d710f24e706bc5fd1aab687796c24
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2023-03-05 20:18:37 -0300

    refactoring repository

--- lib/layers/domain/usecases/get_cars_by_license_plate/get_cars_by_license_plate_usecase_imp.dart
+++ lib/layers/domain/usecases/get_cars_by_license_plate/get_cars_by_license_plate_usecase_imp.dart
@@ -1,13 +1,14 @@
-import '../../../data/dto/car_dto.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:clean_architecture_principles/layers/domain/repositories/get_cars_by_license_plate_repository.dart';
+
 import '../../entities/car_entity.dart';
 import 'get_cars_by_license_plate_usecase.dart';
 
 class GetCarByLicensePlateUseCaseImp extends GetCarByLicensePlateUseCase {
+  final GetCarByLicensePlateRepository repository;
+  GetCarByLicensePlateUseCaseImp(this.repository);
   @override
   CarEntity call(String licensePlate) {
-    if (licensePlate.contains("ABC123")) {
-      return CarDTO(plate: "ABC123", doors: 3, value: 5, color: "red");
-    }
-    return CarDTO(plate: "qwe123", color: '', doors: 3, value: 2);
+    return repository(licensePlate);
   }
 }
