4c32ab3c63902985b49aae114d752b511e054547
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2023-03-05 20:01:09 -0300

    refactor the test

--- test/layers/data/repositories/get_cars_by_license_plate_repository_test.dart
+++ test/layers/data/repositories/get_cars_by_license_plate_repository_test.dart
@@ -1,21 +1,7 @@
-import 'package:clean_architecture_principles/layers/data/dto/car_dto.dart';
+import 'package:clean_architecture_principles/layers/data/repositories/get_cars_by_license_plate_repository_imp.dart';
 import 'package:clean_architecture_principles/layers/domain/entities/car_entity.dart';
+import 'package:clean_architecture_principles/layers/domain/repositories/get_cars_by_license_plate_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-abstract class GetCarByLicensePlateRepository {
-  CarEntity call(String licensePlate);
-}
-
-class GetCarsByLicensePlateRepositoryImp
-    extends GetCarByLicensePlateRepository {
-  @override
-  CarEntity call(String licensePlate) {
-    if (licensePlate.contains("ABC123")) {
-      return CarDTO(plate: "ABC123", doors: 3, value: 5, color: "red");
-    }
-    return CarDTO(plate: "qwe123", color: '', doors: 3, value: 2);
-  }
-}
 
 void main() {
   GetCarByLicensePlateRepository repository =
